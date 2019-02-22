5db56b5e84bfd7b1d71e310d63f5718461ffb9d1
YinHang <pigbayspy@gmail.com>
2019-02-22 18:14:48 +0800

    add local storage

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,18 @@
 const scale_class = "scale-out";
 
+
 window.onload = function ()
 {
     let mode_init = false;
-    let blur_init = 7;
+    let blur_init = 5;
+    chrome.storage.local.get({
+        "FILTER_MODE": mode_init,
+        "BLUR_SCALE": blur_init
+    }, function (result)
+    {
+        mode_init = result["FILTER_MODE"];
+        blur_init = result["BLUR_SCALE"];
+    });
 
     const mode = document.getElementById("mode");
     const blur_range = document.getElementById("blur_range");
@@ -17,20 +26,26 @@ window.onload = function ()
         range_container.classList.add(scale_class);
     }
 
-    mode.addEventListener("change", () => $(range_container).toggleClass(scale_class));
-
-    // chrome.storage.sync.get({"BLUR": true}, function (result)
-    // {
-    //     console.log(result);
-    // });
-    // chrome.storage.sync.get({"BLUR_SCALE":5},function (result)
-    // {
-    //     console.log(result);
-    // });
+    mode.addEventListener("click", () =>
+    {
+        console.log("change");
+        if (range_container.classList.contains(scale_class))
+        {
+            range_container.classList.remove(scale_class);
+        }
+        else
+        {
+            range_container.classList.add(scale_class);
+        }
+    });
 
     save.addEventListener("click", () =>
     {
         let blur_value = blur_range.value;
         let m = mode.checked;
+        chrome.storage.local.set({
+            "FILTER_MODE": m,
+            "BLUR_SCALE": blur_value
+        });
     });
 };
