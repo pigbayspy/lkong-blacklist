2ea27f4ecf71df8543c6ad391092dbd4a0d97b5c
YinHang <pigbayspy@gmail.com>
2019-02-22 17:05:56 +0800

    change popup

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,23 @@
+const scale_class = "scale-out";
+
 window.onload = function ()
 {
+    let mode_init = false;
+    let blur_init = 7;
+
     const mode = document.getElementById("mode");
     const blur_range = document.getElementById("blur_range");
     const range_container = document.getElementById("blur_range_container");
     const save = document.getElementById("save_settings");
 
-    mode.addEventListener("change", function ()
+    blur_range.value = blur_init;
+    mode.checked = mode_init;
+    if (!mode_init)
     {
-        $(range_container).toggleClass("scale-out");
-    });
+        range_container.classList.add(scale_class);
+    }
+
+    mode.addEventListener("change", () => $(range_container).toggleClass(scale_class));
 
     // chrome.storage.sync.get({"BLUR": true}, function (result)
     // {
@@ -18,4 +27,10 @@ window.onload = function ()
     // {
     //     console.log(result);
     // });
+
+    save.addEventListener("click", () =>
+    {
+        let blur_value = blur_range.value;
+        let m = mode.checked;
+    });
 };
