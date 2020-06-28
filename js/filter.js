1f1423095e4724a7aae2b82e269dd7878a293369
yin hang <pigbayspy@gmail.com>
2020-06-28 09:45:47 +0800

    feat:优化代码结构

--- js/filter.js
+++ js/filter.js
@@ -13,29 +13,27 @@ const posting = "st_post";
 //类型
 const childList = "childList";
 
-let mode = true;//true => remover
-let count = 0;//filter count
+/**
+ * true => remover
+ */
+let mode = true;
+/**
+ * filter count
+ */
+let count = 0;
 
-const forum_remover = function (elements, blacklist)
-{
+const forum_remover = function (elements, blacklist) {
     let i = 0;
-    elements.filter(e => e.classList && e.classList.length > 0).forEach(e =>
-    {
-
-        if (e.classList.contains(posting))
-        {
+    elements.filter(e => e.classList && e.classList.length > 0).forEach(e => {
+        if (e.classList.contains(posting)) {
             let author = e.children[0].children[2].children[0].children[0].innerText;
-            if (blacklist.has(author))
-            {
+            if (blacklist.has(author)) {
                 e.remove();
                 ++i;
             }
-        }
-        else if (e.classList.contains(title))
-        {
+        } else if (e.classList.contains(title)) {
             let author = e.children[1].children[0].children[0].innerText;
-            if (blacklist.has(author))
-            {
+            if (blacklist.has(author)) {
                 e.remove();
                 ++i;
             }
@@ -43,26 +41,20 @@ const forum_remover = function (elements, blacklist)
     });
     count += i;
     chrome.runtime.sendMessage({"FILTER_COUNT": count});
-    chrome.storage.local.set({"FILTER_COUNT":count});
+    chrome.storage.local.set({"FILTER_COUNT": count});
 };
 
-const thread_remover = function (elements, blacklist)
-{
-    let i=0;
-    elements.filter(e => e.nodeType === Node.ELEMENT_NODE).forEach(e =>
-    {
-        if (e.parentElement && e.parentElement.id === "thefeed")
-        {
-            Array.prototype.forEach.call(e.children, d =>
-            {
-                if (d === e.firstElementChild || d === e.lastElementChild)
-                {
+const thread_remover = function (elements, blacklist) {
+    let i = 0;
+    elements.filter(e => e.nodeType === Node.ELEMENT_NODE).forEach(e => {
+        if (e.parentElement && e.parentElement.id === "thefeed") {
+            Array.prototype.forEach.call(e.children, d => {
+                if (d === e.firstElementChild || d === e.lastElementChild) {
                     return;
                 }
                 let author = d.querySelector("div>div>div>a>span").innerText;
                 console.log(author);
-                if (blacklist.has(author))
-                {
+                if (blacklist.has(author)) {
                     d.remove();
                     ++i;
                 }
@@ -71,60 +63,52 @@ const thread_remover = function (elements, blacklist)
     });
     count += i;
     chrome.runtime.sendMessage({"FILTER_COUNT": count});
-    chrome.storage.local.set({"FILTER_COUNT":count});
+    chrome.storage.local.set({"FILTER_COUNT": count});
 };
 
 chrome.storage.local.get({
     "FILTER_MODE": mode,
     "FILTER_COUNT": 0
-}, result =>
-{
+}, result => {
     mode = result["FILTER_MODE"];
     count = result["FILTER_COUNT"];
 
-    if (mode)
-    {
-        fetch("/setting/index.php?mod=ajax&action=getblack").then(response =>
-        {
+    if (mode) {
+        // 获取黑名单
+        fetch("/setting/index.php?mod=ajax&action=getblack").then(response => {
             return response.text();
-        }).then(text =>
-        {
+        }).then(text => {
+            // 解析
             let p = new DOMParser();
             let html = p.parseFromString(text, "text/html");
             let elements = html.querySelectorAll("table>tbody>tr>td>a");
             return new Set(Array.prototype.map.call(elements, e => e.innerText));
-        }).then(blacklist =>
-        {
-            let observer = new MutationObserver(list =>
-            {
+        }).then(blacklist => {
+            let observer = new MutationObserver(list => {
                 let callback;
-                switch (window.location.pathname.split("/")[1])
-                {
-                    case "forum":
-                    {
+                switch (window.location.pathname.split("/")[1]) {
+                    case "forum": {
                         callback = forum_remover;
                         break;
                     }
-                    case "thread":
-                    {
+                    case "thread": {
                         callback = thread_remover;
                         break;
                     }
-                    default:
-                    {
+                    default: {
                         callback = null;
                         break;
                     }
                 }
-                list.forEach(m =>
-                {
-                    if (m.type === childList && callback !== null)
-                    {
+                list.forEach(m => {
+                    if (m.type === childList && callback !== null) {
                         callback(Array.from(m.addedNodes), blacklist);
                     }
                 })
             });
             observer.observe(document, config);
+        }).catch(err => {
+            console.error("请求黑名单异常", err);
         });
     }
 });
--- js/popup.js
+++ js/popup.js
@@ -1,44 +1,31 @@
 let filter_count = 0;
 let mode_init = false;
 
-window.onload = function ()
-{
+window.onload = function () {
     M.AutoInit();
 
     const mode = document.getElementById("mode");
-    const save = document.getElementById("save_settings");
     const count = document.getElementById("filter_count");
-
-    let tooltip = M.Tooltip.init(save, {
-        enterDelay: 100,
-        margin: 2,
-        position: "right",
-        outDuration: 150
-    }, false, false);
 
     chrome.storage.local.get({
         "FILTER_MODE": mode_init,
         "FILTER_COUNT": filter_count
-    }, result =>
-    {
+    }, result => {
         mode_init = result["FILTER_MODE"];
         filter_count = result["FILTER_COUNT"];
         mode.checked = mode_init;
         count.innerText = filter_count;
     });
 
-    save.addEventListener("click", () =>
-    {
-        let m = mode.checked;
+    mode.addEventListener("click", () => {
+        const m = mode.checked;
         chrome.storage.local.set({
             "FILTER_MODE": m
         });
     });
 
-    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse)
-    {
-        if (message["FILTER_COUNT"])
-        {
+    chrome.runtime.onMessage.addListener(function (message, sender, _) {
+        if (message["FILTER_COUNT"]) {
             filter_count = message["FILTER_COUNT"];
             count.innerText = filter_count;
         }
